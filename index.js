676e6188f233bdc2c36a12fdaf47d988182882d7
Ari Siitonen <ext.ari.siitonen@ruokavirasto.fi>
2021-04-21 19:34:47 +0300

    works both ways

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var mqtt = require('mqtt')
-
+const uuid = require('uuid');
 const match = require('mqtt-match');
 
 class MQTTapi {
@@ -17,6 +17,7 @@ class MQTTapi {
   }
   
   publish(path, obj, options = {}) {
+    //console.log(">>",path, obj);
     try {
       this._client.publish(path, JSON.stringify(obj), options);      
     } catch (error) {
@@ -39,12 +40,34 @@ class MQTTapi {
   registerAPI(path, cb) {
     this.apis[path] = {"f": cb};
   }
+
+  req(target, msg, options, cb ) {
+    var obj = {
+      'mid': uuid.v4(),
+      'src': this._id,
+      'target': target,
+      'req': { args: msg },
+    };
+    this.reqs[obj['mid']] = {
+      'obj': obj,
+      'cb': cb,
+      'done': false,
+      'created': MQTTapi.stamp(),
+      'sent': MQTTapi.stamp(),
+      'tries': 1,
+      'retries': "retries" in options ? options.tries : 5,
+      'timeout': "timeout" in options ? options.timeout : 3000
+    };
+    console.log("reqs", this.reqs);
+    this.publish(`/dn/${target}/${obj['mid']}`, obj);
+  }
   
   constructor(url, id) {
     var ctx = this;
     this._id = id;
     this.sublist = {};
     this.apis = {};
+    this.reqs = {};
 
     var _client = mqtt.connect(url, {
       will: {
@@ -74,10 +97,21 @@ class MQTTapi {
     }.bind(this))
 
     this.subscribe("/bc/+/+", (topic, msg) => {
-      console.log("bc:", topic, msg);
+      //console.log("bc:", topic, msg);
     })
 
+
+    this.subscribe(`/up/${this._id}/#`, (topic, obj) => {
+      //console.log("got reply", topic, obj);
+      if (obj['mid'] in this.reqs) {
+        var r = this.reqs[obj['mid']]
+        r.done = true;
+        if (r.cb)
+          r.cb(null, obj.reply)
+      }
+    })
     this.subscribe(`/dn/${this._id}/#`, (topic, msg) => {
+      //console.log("got <<",topic, msg);
       if (msg['req']['args'][0] in this.apis) {
         var api = this.apis[msg['req']['args'][0]];
         
@@ -105,6 +139,27 @@ class MQTTapi {
     this.registerAPI("ping", (msg) => { 
       return {"pong": true};
     })
+    setInterval(() => {
+      for (var k in this.reqs) { 
+        var r = this.reqs[k]
+        const now = MQTTapi.stamp()
+        if (r.done)
+          delete this.reqs[k]
+        else if (now > r.sent + r.timeout) {
+          if (r.retries > r.tries) {
+            console.log("retry");
+            r.tries += 1;
+            r.obj.resend = r.tries
+            this.publish(`/dn/${r.target}/${r['mid']}`, r.obj);
+            r.sent = now;
+          } else {
+            if (r.cb)
+              r.cb("timeout")
+            r.done = true;
+          }
+        }
+      }
+    }, 100);
   }
 }
 
--- test.js
+++ test.js
@@ -1,3 +1,20 @@
 var rt0s = require('./index.js');
 
 mq = new rt0s('mqtt://mqtt.rt0s.com', 'libtester');
+
+mq.registerAPI("pung", (msg) => { 
+  return {"pung": true};
+})
+
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+sleep(500).then(() => {
+  mq.req("numnet", ["ping"], {}, (err,ret) => { 
+    console.log("got reply",err,ret);
+  })
+  mq.req("numnetx", ["ping"], {} , (err,ret) => { 
+    console.log("got reply",err,ret);
+  })
+});
