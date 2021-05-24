48fe89c558b73b90e3055feb33665b519ade8bbb
Ari Siitonen <jalopuuverstas@gmail.com>
2021-05-24 21:22:16 +0300

    fix3

--- index.js
+++ index.js
@@ -60,6 +60,7 @@ class MQTTapi {
 
   end() {
     this._client.end();
+    cha
     this.change_state(false)
   }
 
@@ -142,7 +143,7 @@ class MQTTapi {
       console.error("err..");
     })
 
-    var change_state = (newstate) => {
+    var change_state = this.change_state = (newstate) => {
       if (this.connected == newstate && newstate==false) {
         console.log("double off -- logout ");
         return;
